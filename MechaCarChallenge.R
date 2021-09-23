661f2f779cd3507f658e42caf3af2aa9c9c172d4
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-09-23 16:26:54 -0700

    Update r script

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -8,3 +8,8 @@ library(dplyr)
 # Read in MechaCar_mpg.csv as data frame
 MechaCar_mpg <-read_csv("MechaCar_mpg.csv")
 
+# Linear regression on all 6 variables
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg)
+
+# p-value and r-squared value of regression
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
