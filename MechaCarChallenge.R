b6e0360bbcca2995422d75747b23019234810ea5
aseo67 <85456812+aseo67@users.noreply.github.com>
2021-09-27 19:57:11 -0700

    Update analysis

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,6 +1,6 @@
 # Module 15 Challenge
 
-# Deliverable 1
+# Deliverable 1: Linear Regression to Predict MPG
 
 # Download dplyr package
 library(dplyr)
@@ -11,7 +11,7 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 # p-value and r-squared value of regression
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=MechaCar_mpg))
 
-# Deliverable 2
+# Deliverable 2: Summary Statistics on Suspension Coils
 
 # Read in MechaCar_mpg.csv as table
 Suspension_Coil <-read_csv("Suspension_Coil.csv")
@@ -20,7 +20,7 @@ total_summary <- Suspension_Coil %>% summarize(Mean = mean(PSI), Median = median
 # Create "lot_summary" dataframe, using group_by() & summarize() functions, of suspension coil’s PSI
 lot_summary <- Suspension_Coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean = mean(PSI), Median = median(PSI), Variance = var(PSI), SD = sd(PSI))
 
-# Deliverable 3
+# Deliverable 3: T-Tests on Suspension Coils
 
 # Run t-test to determine if PSI across all lots is statistically different from population mean of 1,500 pounds per square inch.
 t.test(Suspension_Coil$PSI, mu=1500)
@@ -28,6 +28,11 @@ t.test(Suspension_Coil$PSI, mu=1500)
 t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot1"), mu=1500)
 t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot2"), mu=1500)
 t.test(subset(Suspension_Coil$PSI, Suspension_Coil$Manufacturing_Lot == "Lot3"), mu=1500)
+
+# Deliverable 4: Study Design - MechaCar vs. Competition
+
+
+
 
 
 # Deliverable 4
